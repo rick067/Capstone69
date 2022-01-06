80ec5dc06a0b9a67017c950d31bdaf480df1e1ef
rick067 <rickrodis067@gmail.com>
2022-01-06 15:36:10 +0800

    Warm-up Exercise update

--- app/src/main/java/com/example/capstone/MainMenu.java
+++ app/src/main/java/com/example/capstone/MainMenu.java
@@ -3,6 +3,7 @@ package com.example.capstone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        //FULL Screen Display
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                  |  View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                  |  View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                  |  View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                  |  View.SYSTEM_UI_FLAG_FULLSCREEN
+                  |  View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
     }
 }
