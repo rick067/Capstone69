a43905b6f58a2e92d17b321921beb9e3f5056aa9
rick067 <rickrodis067@gmail.com>
2022-03-21 20:29:41 +0800

    Warm-up Exercise timer update

--- app/src/main/java/com/example/capstone/MainMenu.java
+++ app/src/main/java/com/example/capstone/MainMenu.java
@@ -2,15 +2,29 @@ package com.example.capstone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        button = (Button) findViewById(R.id.button6);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openExerciseList();
+            }
+        });
+
+
+
 
         //FULL Screen Display
         this.getWindow().getDecorView().setSystemUiVisibility(
@@ -21,5 +35,10 @@ public class MainMenu extends AppCompatActivity {
                   |  View.SYSTEM_UI_FLAG_FULLSCREEN
                   |  View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
+    }
+
+    private void openExerciseList() {
+        Intent intent = new Intent( this, ExerciseList.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/capstone/User.java
@@ -0,0 +1,7 @@
+package com.example.capstone;
+
+public class User {
+
+    
+
+}
